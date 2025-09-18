8dc1739c6c02549d37bb7d5ffa646f3f29fd8964
potocki92 <mateusz.potocki92@gmail.com>
2025-09-18 15:51:16 +0200

    Update request.ts

--- src/i18n/request.ts
+++ src/i18n/request.ts
@@ -1,10 +1,11 @@
-import {getRequestConfig} from 'next-intl/server';
- 
-export default getRequestConfig(async () => {
-  const locale = 'en';
- 
+import { getRequestConfig } from "next-intl/server";
+
+export default getRequestConfig(async ({ locale }) => {
+  // fallback
+  const currentLocale = locale ?? "en";
+
   return {
-    locale,
-    messages: (await import(`../../messages/${locale}.json`)).default
+    locale: currentLocale,
+    messages: (await import(`../../messages/${currentLocale}.json`)).default,
   };
 });
