48c7e340bb387e8bba5dfd6fa414b0076ed9fdc9
potocki92 <mateusz.potocki92@gmail.com>
2025-09-18 15:49:04 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,11 @@
+// next.config.js
 const createNextIntlPlugin = require('next-intl/plugin');
- 
-const withNextIntl = createNextIntlPlugin();
- 
+
+const withNextIntl = createNextIntlPlugin('./src/i18n/request.ts');
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
- 
+const nextConfig = {
+  reactStrictMode: true
+};
+
 module.exports = withNextIntl(nextConfig);
